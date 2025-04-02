2f102e1f2b8eda1aab925dae7bee7fad9457f90c
Benny Colacion <b.colacion.536329@umindanao.edu.ph>
2025-04-02 16:55:22 +0800

    Added task 2 and task 3 in lab 3

--- lab3/Form1.Designer.cs
+++ lab3/Form1.Designer.cs
@@ -20,20 +20,52 @@
 			base.Dispose(disposing);
 		}
 
-		#region Windows Form Designer generated code
+        #region Windows Form Designer generated code
 
-		/// <summary>
-		///  Required method for Designer support - do not modify
-		///  the contents of this method with the code editor.
-		/// </summary>
-		private void InitializeComponent()
-		{
-			this.components = new System.ComponentModel.Container();
-			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-			this.ClientSize = new System.Drawing.Size(800, 450);
-			this.Text = "Form1";
-		}
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            listBoxBooks = new ListBox();
+            btnShowBooks = new Button();
+            SuspendLayout();
+            // 
+            // listBoxBooks
+            // 
+            listBoxBooks.FormattingEnabled = true;
+            listBoxBooks.ItemHeight = 15;
+            listBoxBooks.Location = new Point(125, 21);
+            listBoxBooks.Name = "listBoxBooks";
+            listBoxBooks.Size = new Size(515, 169);
+            listBoxBooks.TabIndex = 0;
+            // 
+            // btnShowBooks
+            // 
+            btnShowBooks.Location = new Point(16, 21);
+            btnShowBooks.Name = "btnShowBooks";
+            btnShowBooks.Size = new Size(103, 33);
+            btnShowBooks.TabIndex = 1;
+            btnShowBooks.Text = "Show";
+            btnShowBooks.UseVisualStyleBackColor = true;
+            btnShowBooks.Click += btnShowBooks_Click;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(652, 450);
+            Controls.Add(btnShowBooks);
+            Controls.Add(listBoxBooks);
+            Name = "Form1";
+            Text = "Form1";
+            ResumeLayout(false);
+        }
 
-		#endregion
-	}
+        #endregion
+
+        private ListBox listBoxBooks;
+        private Button btnShowBooks;
+    }
 }
--- lab3/Form1.cs
+++ lab3/Form1.cs
@@ -1,10 +1,91 @@
+using System.Reflection.Metadata.Ecma335;
+
 namespace lab3
 {
-	public partial class Form1 : Form
-	{
-		public Form1()
-		{
-			InitializeComponent();
-		}
-	}
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        public class Book
+        {
+            public string Title { get; set; }
+
+            public string Author { get; set; }
+
+            public virtual string GetInfo()
+            {
+                return $"{Title} by {Author}";
+            }
+        }
+
+
+        public class Magazine : Book
+        {
+            public int IssueNumber { get; set; }
+
+            public override string GetInfo()
+            {
+                return $"{Title} - Issue #{IssueNumber}";
+            }
+        }
+
+        //Task 2
+        public class Ebook : Book
+        {
+            public string Format { get; set; }
+
+            public override string GetInfo()
+            {
+                return $"{Title} by {Author} (Format: {Format})";
+            }
+        }
+
+        public void ProcessBooks(Book[] books)
+        {
+            listBoxBooks.Items.Clear();
+
+            foreach (Book book in books)
+            {
+                listBoxBooks.Items.Add(book.GetInfo());
+            }
+        }
+
+        private void btnShowBooks_Click(object sender, EventArgs e)
+        {
+            Book[] books = new Book[]
+            {
+                new Book { Title = "System Design", Author = "Jay Al Alfonso" },
+                new Magazine { Title = "Best Models Boys", IssueNumber = 45 },
+                new Ebook { Title = "Calculus 2", Author = "Torres Guirerra", Format = "PDF" },
+                new Textbook { Title = "Calculus 1", Author = "Albert Einstein", Subject = "CEE101"},
+                new AudioBook { Title = "Differential Equation", Author = "Mr. Bean", Narrator = "Rowan Atkinson", Duration = 5.5}
+            };
+
+            ProcessBooks(books);
+        }
+
+        public class Textbook : Book
+        {
+            public string Subject { get; set; }
+
+            public override string GetInfo()
+            {
+                return $"{Title} by {Author} (Subject: {Subject})";
+            }
+        }
+
+        public class AudioBook : Book
+        {
+            public double Duration { get; set; }
+            public string Narrator { get; set; }
+
+            public override string GetInfo()
+            {
+                return $"{Title} by {Author} (Narrated by {Narrator}, Duration: {Duration} hrs)";
+            }
+        }
+    }
 }
